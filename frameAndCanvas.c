fb88b1776a26b509965963e6387c93fa99b7eb29
Jason LaFerrera <jason.laferrera@food52.com>
2024-12-23 10:57:35 -0500

    test

--- frameAndCanvas.c
+++ frameAndCanvas.c
@@ -10,7 +10,8 @@ int main() {
     clear_framebuffer(0x000000);                                                // Clear to black
     fill_rect(rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT, 50, 50, 0xFF0000); // Draw red squares
     render_framebuffer();
-    usleep(16667); // ~60 FPS
+    clear_framebuffer(0xFF0000);
+    // usleep(16667); // ~60 FPS
   }
 
   cleanup_framebuffer();
